a71c264fb966a634939195fa8b0ab6fdd8d2f40b
Alen Badel <Alen.Badel@ibm.com>
2018-08-09 13:11:00 -0400

    Only setup and run neo4j

--- runAll.sh
+++ runAll.sh
@@ -6,11 +6,5 @@ BENCHMARK=${3-`pwd`}
 DBFOLDER=${4-`pwd`/databases}
 RUSER=${5-ubuntu}
 
-./runTest.sh arangodb $TEST_IP $LOOP_COUNT $BENCHMARK $DBFOLDER $RUSER
-./runTest.sh arangodb_mmfiles $TEST_IP $LOOP_COUNT $BENCHMARK $DBFOLDER $RUSER
+echo "Only Neo4J is supported"
 ./runTest.sh neo4j $TEST_IP $LOOP_COUNT $BENCHMARK $DBFOLDER $RUSER
-./runTest.sh orientdb $TEST_IP $LOOP_COUNT $BENCHMARK $DBFOLDER $RUSER
-./runTest.sh postgresql_jsonb $TEST_IP $LOOP_COUNT $BENCHMARK $DBFOLDER $RUSER
-./runTest.sh postgresql_tabular $TEST_IP $LOOP_COUNT $BENCHMARK $DBFOLDER $RUSER
-./runTest.sh mongodb $TEST_IP $LOOP_COUNT $BENCHMARK $DBFOLDER $RUSER
-
